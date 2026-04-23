323118a2dd458e19a42311393672c01626328584
Brian <blammerse@it-legends.nl>
2026-04-23 16:50:53 +0300

    Feat: Additional service collection extensions to configure service accounts (#8)

--- src/Zitadel.AccessTokenManagement/Extensions/ServiceCollectionExtensions.cs
+++ src/Zitadel.AccessTokenManagement/Extensions/ServiceCollectionExtensions.cs
@@ -27,6 +27,27 @@ public static class ServiceCollectionExtensions
         this IServiceCollection services,
         Action<ZitadelServiceAccount> configureOptions)
         => services.ConfigureZitadelServiceAccount(ZitadelClientConstants.DefaultServiceAccountName, configureOptions);
+    
+    /// <summary>
+    /// Configures the default Zitadel service account with the specified options.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the service account configuration to.</param>
+    /// <param name="configureOptions">A callback to configure the <see cref="ZitadelServiceAccount"/> object.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/> instance.</returns>
+    public static IServiceCollection ConfigureZitadelServiceAccount<T1>(
+        this IServiceCollection services,
+        Action<ZitadelServiceAccount, T1> configureOptions) where T1 : class
+        => services.ConfigureZitadelServiceAccount(ZitadelClientConstants.DefaultServiceAccountName, configureOptions);
+    
+    /// <summary>
+    /// Bind the default Zitadel service account to a configuration section.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the service account configuration to.</param>
+    /// <param name="configurationSectionPath">The name of the configuration section to bind from</param>
+    public static IServiceCollection ConfigureZitadelServiceAccount(
+        this IServiceCollection services,
+        string configurationSectionPath)
+        => services.ConfigureZitadelServiceAccount(ZitadelClientConstants.DefaultServiceAccountName, configurationSectionPath);
 
     /// <summary>
     /// Configures a named Zitadel service account with the specified options.
@@ -40,7 +61,51 @@ public static class ServiceCollectionExtensions
         string serviceAccountName,
         Action<ZitadelServiceAccount> configureOptions)
     {
-        services.Configure(serviceAccountName, configureOptions);
+        services
+            .AddSharedServices(serviceAccountName)
+            .AddOptions<ZitadelServiceAccount>(serviceAccountName)
+            .Configure(configureOptions);
+        return services;
+    }
+
+    /// <summary>
+    /// Configures a named Zitadel service account with the specified options.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the service account configuration to.</param>
+    /// <param name="serviceAccountName">The name of the service account being configured.</param>
+    /// <param name="configureOptions">A callback to configure the <see cref="ZitadelServiceAccount"/> object.</param>
+    /// <returns></returns>
+    public static IServiceCollection ConfigureZitadelServiceAccount<T1>(this IServiceCollection services,
+        string serviceAccountName,
+        Action<ZitadelServiceAccount, T1> configureOptions) where T1 : class
+    {
+        services
+            .AddSharedServices(serviceAccountName)
+            .AddOptions<ZitadelServiceAccount>(serviceAccountName)
+            .Configure(configureOptions);
+        return services;
+    }
+
+    /// <summary>
+    /// Bind a named Zitadel service account to a configuration section.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the service account configuration to.</param>
+    /// <param name="serviceAccountName">The name of the service account being configured.</param>
+    /// <param name="configurationSectionPath">The name of the configuration section to bind from</param>
+    /// <returns></returns>
+    public static IServiceCollection ConfigureZitadelServiceAccount(this IServiceCollection services,
+        string serviceAccountName,
+        string configurationSectionPath)
+    {
+        services
+            .AddSharedServices(serviceAccountName)
+            .AddOptions<ZitadelServiceAccount>(serviceAccountName)
+            .BindConfiguration(configurationSectionPath);
+        return services;
+    }
+
+    private static IServiceCollection AddSharedServices(this IServiceCollection services, string serviceAccountName)
+    {
         services.TryAddSingleton<IValidateOptions<ZitadelServiceAccount>, ZitadelServiceAccountValidator>();
         services.TryAddSingleton<IClientAssertionService, ZitadelClientAssertionService>();
         services.TryAddSingleton<ZitadelClientAssertionMessageHandler>();
